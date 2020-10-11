314e72c3aa20871e23348e079c76e96d9a8cd869
Sergei827 <marchukcha89@gmail.com>
2020-10-11 17:22:26 +0300

    my nix solution hello

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+$hello='Hello, ';
+$nix='NIX ';
+$education='Education';
+
+$nixEdClass='hello-nix-box';
+$helloClass='hello-class';
+$nixClass='nix-class';
+$educationClass='education';
+
+require_once "temp/hello_nix.php";
+
+?>
---
+++ temp/hello_nix.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset='utf-8'>
+	<title>hello NIX</title>
+	<link href="css/nix_style.css" rel='stylesheet'>
+</head>
+<body>
+<div class='<?php echo $nixEdClass ?>'>
+    <span class='<?php echo $helloClass ?>'><?php echo $hello ?></span>
+    <span class='<?php echo $nixClass ?>'><?php echo $nix ?></span>
+    <span class='<?php echo $educationClass ?>'><?php echo $education ?></span>
+</div>
+</body>
+</html>
